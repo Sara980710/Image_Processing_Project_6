255e8503409c1fdd78019cf5a987e4f26a99923a
Sara980710 <sara@idala.eu>
2021-09-24 15:50:01 +0200

    restricted to remove only on sky

--- EdgeDetectionWithMedianFilter.m
+++ EdgeDetectionWithMedianFilter.m
@@ -9,8 +9,8 @@ title('original image')
 
 % Greyscale
 grey = rgb2gray(im);
-figure, imshow(grey, [])
-title('greyscale image')
+%figure, imshow(grey, [])
+%title('greyscale image')
 
 % Gaussian filter
 filterSize = 2;
@@ -39,19 +39,88 @@ GmagBinary = bwareaopen(GmagBinary,10);
 figure, imshow(GmagBinary, [])
 title('Only big dotts')
 
+% Get color channels
+R = im(:,:,1);
+G = im(:,:,2);
+B = im(:,:,3);
+
+% Detect buidlings/background
+im_subtracted_RB = R-B;
+figure
+title('Color subtracted image')
+subplot(3,3,1),imshow(im_subtracted_RB, [])
+title('R-B')
+im_subtracted_RG = R-G;
+subplot(3,3,2),imshow(im_subtracted_RG, [])
+title('R-G')
+im_subtracted = R-R;
+subplot(3,3,3),imshow(im_subtracted, [])
+title('R-R')
+im_subtracted = G-B;
+subplot(3,3,4),imshow(im_subtracted, [])
+title('G-B')
+im_subtracted = G-G;
+subplot(3,3,5),imshow(im_subtracted, [])
+title('G-G')
+im_subtracted_GR = G-R;
+subplot(3,3,6),imshow(im_subtracted_GR, [])
+title('G-R')
+im_subtracted = B-B;
+subplot(3,3,7),imshow(im_subtracted, [])
+title('B-B')
+im_subtracted = B-G;
+subplot(3,3,8),imshow(im_subtracted, [])
+title('B-G')
+im_subtracted = B-R;
+subplot(3,3,9),imshow(im_subtracted, [])
+title('B-R')
+
+% G-R since the scratches are visible
+im_norm = im_subtracted_GR./max(max(im_subtracted_GR));
+%figure, imhist(double(im_norm))
+im_median = median(median(im_norm));
+im_binary = im2bw(im_subtracted_GR, im_median);
+figure
+subplot(1,3,1),imshow(im_binary, [])
+title('Color subtracted image thresholded G-R')
+
+% R-B
+im_norm = im_subtracted_RB./max(max(im_subtracted_RB));
+%figure, imhist(double(im_norm))
+im_median = median(median(im_norm));
+im_binary = im2bw(im_subtracted_RB, im_median/2);
+figure
+subplot(1,3,1),imshow(im_binary, [])
+title('Color subtracted image thresholded R-B')
+
+% R-G
+im_norm = im_subtracted_RG./max(max(im_subtracted_RG));
+%figure, imhist(double(im_norm))
+im_median = median(median(im_norm));
+im_binary_2 = im2bw(im_subtracted_RG, im_median);
+subplot(1,3,2), imshow(im_binary_2, [])
+title('Color subtracted image thresholded R-G')
+
+% Combine and dilate
+im_binary = im_binary_2 | im_binary;
+se = strel('diamond',6);
+im_binary = imdilate(im_binary,se);
+subplot(1,3,3), imshow(im_binary, [])
+title('Color subtracted image thresholded combined')
+
+maskImage = ~im_binary & GmagBinary;
+figure, imshow(maskImage, [])
+title('Mask')
+
 % try determine the color of the screatches
-area = GmagBinary;
+area = maskImage;
 area = im2uint8(area);
 area(area > 0) = 1;
 predScratchArea = im .* area;
 figure, imshow(predScratchArea, [])
-title('Scratch area in color')
+title('Mask in color')
 
 % Smooth image
-maskImage = GmagBinary;
-R = im(:,:,1);
-G = im(:,:,2);
-B = im(:,:,3);
 medfilimgR = medfilt2(R, [10,10]);
 medfilimgG = medfilt2(G, [10,10]);
 medfilimgB = medfilt2(B, [10,10]);
